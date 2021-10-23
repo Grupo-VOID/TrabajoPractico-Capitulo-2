01b13af6006e33e5130e9fdf799094073b7f69c3
maurobustos <bustosrme@gmail.com>
2021-10-23 17:06:54 -0300

    Corrección en la interfaz de AtraccionesDAO

--- TrabajoPractico-Capitulo-2/src/main/java/dao/AtraccionesDAO.java
+++ TrabajoPractico-Capitulo-2/src/main/java/dao/AtraccionesDAO.java
@@ -1,10 +1,12 @@
 package dao;
 
+import java.sql.SQLException;
+
 import model.Atraccion;
 
 public interface AtraccionesDAO extends GenericDAO<Atraccion> {
 
-	public int updateCupoActual(Atraccion atraccion);
+	public int updateCupoActual(Atraccion atraccion) throws SQLException;
 
 	public Atraccion buscarPorNombre(String nombre);
 
