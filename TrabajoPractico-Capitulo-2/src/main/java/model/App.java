cc2ed8291dfe24962fbc649fb37b4e72a6b3d37a
maurobustos <bustosrme@gmail.com>
2021-10-23 17:43:58 -0300

    Habilitacion de la clase main App
    Merge branch 'master' of
    https://github.com/Grupo-VOID/TrabajoPractico-Capitulo-2.git

--- TrabajoPractico-Capitulo-2/src/main/java/model/App.java
+++ TrabajoPractico-Capitulo-2/src/main/java/model/App.java
@@ -1,39 +1,24 @@
 package model;
 
-import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.LinkedList;
 
 import dao.AtraccionDAO;
 import dao.DAOFactory;
-import dao.MissingDataException;
+import dao.PromocionDAO;
 import dao.UsuarioDAO;
-import jdbc.ConnectionProvider;
 
 public class App {
 
-//	public static void main(String[] args) throws IOException {
-
-//			ParqueAtracciones parque = new ParqueAtracciones();
-//			parque.agregarUsuarios(LectorArchivos.GenerarUsuarios("entrada/usuarios.txt"));
-//			parque.agregarAtracciones(LectorArchivos.GenerarAtracciones("entrada/atracciones.txt"));
-//			parque.agregarPromociones(LectorArchivos.GenerarPromociones(parque, "entrada/promociones.txt"));
-//			parque.cargarCatalogo();
-//
-//			UI.eleccionDeMenu(parque); // Método para elegir modo de compra
-//		}
-//	}
 	public static void main(String[] args) throws SQLException {
 		AtraccionDAO atraccionesDAO = DAOFactory.getAtraccionesDAO();
-		System.out.println(atraccionesDAO.findAll());
-}
-//	
-//	public static void main(String[] args) throws SQLException {
-//	UserDAO userDAO = DAOFactory.getUserDAO();
-//	System.out.println(userDAO.findAll());
-//}
+		UsuarioDAO usuarioDAO = DAOFactory.getUserDAO();
+		PromocionDAO promocionesDAO = DAOFactory.getPromocionesDAO();
+
+		ParqueAtracciones parque = new ParqueAtracciones();
+		parque.agregarUsuarios(usuarioDAO.findAll());
+		parque.agregarAtracciones(new LinkedList<Adquirible>(atraccionesDAO.findAll()));
+		parque.agregarPromociones(new LinkedList<Adquirible>(promocionesDAO.findAll()));
+		parque.cargarCatalogo();
+	}
 }
